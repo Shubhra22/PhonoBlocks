9bc06407524c48b46f4abf332b6163289f05e652
Shubhra Sarker <ssuvro22@outlook.com>
2020-02-29 23:17:05 -0800

    Initialize Project

---
+++ Assets/Scripts/ColorText.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorText : MonoBehaviour
+{
+
+    // Needed for applying color to the word history bar 
+    // Method is called in LetterGridController class
+    // Takes a plane text as input and color each letter based on the colors array
+    public static string TextToColor(string text, Color[] colors)
+    {
+        string output = "";
+        if (!String.IsNullOrEmpty(text))
+        {           
+            for (int i = 0; i < text.Length; i++)
+            {
+                output += "<color=#" + ColorUtility.ToHtmlStringRGBA(colors[i]) + ">"+text[i]+"</color>";
+            }
+        }
+        return output;
+    }
+
+    public static string TextToColor(string text, Color[] colors, int flash)
+    {
+        string output = "";
+        if (!String.IsNullOrEmpty(text))
+        {           
+            for (int i = 0; i < text.Length; i++)
+            {
+                output += "<color=#" + ColorUtility.ToHtmlStringRGBA(colors[i]) + ">"+text[i]+"</color>";
+            }
+        }
+        return output;
+    }
+    
+}
---
+++ Assets/Scripts/Rules.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Rules : MonoBehaviour
+{
+    [SerializeField]
+    private Color[] cbc;
+    
+    public Text word;
+
+    private string ruleStr;
+
+    private string resultStr;
+    void Update()
+    {
+        foreach(KeyCode vKey in System.Enum.GetValues(typeof(KeyCode)))
+        {
+            if(Input.GetKeyUp(vKey))
+            {
+                resultStr += vKey.ToString();
+            }
+        }
+
+        //word.text = resultStr;
+        CBC(resultStr);
+    }
+
+    void CBC(String s)
+    {
+        word.text = ColorText.TextToColor(s, cbc);
+    }
+
+    void Blend(string[] blends)
+    {
+        
+    } 
+}
